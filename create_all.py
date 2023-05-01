10e3801bf7412b90c169bad812ed573e4739bcdb
anton264 <anton.eriksmo@gmail.com>
2023-05-01 11:35:46 +0200

    Refined comparison

--- create_all.py
+++ create_all.py
@@ -12,8 +12,8 @@ preload_models()
 
 # Looks through all subdirectories of a path and returns the paths to every csv file with forward slash
 workfolder = 'D:/aiJane/voice/numbers'
-voice_threshold = 80
-max_retries = 5
+voice_threshold = 70
+max_retries = 20
 
 csvs = find_csvs(workfolder)
 
@@ -28,28 +28,31 @@ create_or_reuse_csv_with_all_data(data, all_data_path)
 
 # Create audio files from the data
 for d in data:
-    print(d[0], d[1])
+    # print(d[0], d[1])
     # Check if the file already has a score
     raw_value = get_value_from_column(all_data_path, d[0], 4)
     previous_score = int(raw_value) if raw_value is not None else 0
     # Skip if the score is already above voice_threshold
-    if previous_score > voice_threshold:
-        print("✅ Skipping", d[0], "because it already has a score of", previous_score, ", which is above the threshold of ", voice_threshold)
+    if previous_score >= voice_threshold:
+        print("✅ Skipping", d[0], "because it already has a score of",
+              previous_score, ", which is above the threshold of ", voice_threshold)
         continue
     score = 0
     # Check if the file already exists, if it does, check the score
     if os.path.isfile(d[0]) and previous_score == 0:
         voice_recognizer = sr.perform_voice_recognition(d[0])
-        score = sr.get_similarity_score(voice_recognizer, d[1])
+        score = sr.get_similarity_score(voice_recognizer.lower(), d[1].lower())
         print(f"The file {d[0]} already exists and has a score of {score}")
     retries = 0
     while score < voice_threshold:
-        generate_sound_file(d[0], d[1], 0.1)
+        generate_sound_file(d[0], d[1])
         voice_recognizer = sr.perform_voice_recognition(d[0])
         score = sr.get_similarity_score(voice_recognizer.lower(), d[1].lower())
-        print(f"💬 The file: {d[0]}👂 I heard: {voice_recognizer} \n📖 It should be: {d[1]} \n💯 It scored: {score} points\n")
         retries += 1
-        if retries > max_retries:
+        print(f"💬 The file: {d[0]}\n👂 I heard: {voice_recognizer} \n📖 It should be: {d[1]} \n💯 It scored: {score} points\n🔢 This was attempt number {retries} out of {max_retries}")
+        if retries >= max_retries:
+            print(
+                f"🚫 The phrase: {d[1]} was skipped because it failed to reach the threshold of {voice_threshold} after {max_retries} retries")
             break
     # Save score to csv
     insert_value_in_column(all_data_path, d[0], score, 4)
--- speech_recognition.py
+++ speech_recognition.py
@@ -1,5 +1,8 @@
+import re
 import whisper
+from pyphonetics import Metaphone
 from fuzzywuzzy import fuzz
+from num2words import num2words
 
 
 def perform_voice_recognition(file_path):
@@ -22,6 +25,38 @@ def get_similarity_score(text: str, expected_result: str) -> int:
     """
     Returns a number between 0 and 100, where 100 is a perfect match
     """
-    match = fuzz.ratio(text, expected_result)
+    metaphone = Metaphone()   
+    text = convert_number_to_words(text)
+    expected_result = convert_number_to_words(expected_result)
+    text = remove_special_chars(text)
+    expected_result = remove_special_chars(expected_result)
+    # Compare with metaphone phonetics
+    match = fuzz.ratio(metaphone.phonetics(text), metaphone.phonetics(expected_result))
     return match
 
+def convert_number_to_words(s: str) -> str:
+    def is_number(word: str) -> bool:
+        return re.match(r'^-?\d+(\.\d+)?$', word)
+
+    def split_leading_zero(word: str) -> str:
+        if word.startswith('0') and len(word) > 1:
+            return '0 ' + word[1:]
+        return word
+
+    words = s.split()
+    converted_words = []
+
+    for word in words:
+        word = split_leading_zero(word)
+        if is_number(word):
+            word = num2words(float(word))
+        converted_words.append(word)
+
+    return ' '.join(converted_words)
+
+
+def remove_special_chars(text: str) -> str:
+    """
+    Removes special characters and punctuation from the input string.
+    """
+    return re.sub(r"[^\w\s]", "", text)    
